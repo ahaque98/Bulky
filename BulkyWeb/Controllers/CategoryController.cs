70612fcbdbe4a98b3511a4a1d79495dc9ceb2b83
vikingslord <vikingslord98@gmail.com>
2023-09-07 17:07:38 +0600

    added Tempdata[] notification

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -38,6 +38,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category added successfully!";
                 return RedirectToAction("Index");
             }
 
@@ -77,6 +78,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
                 return RedirectToAction("Index");
             }
 
@@ -109,7 +111,7 @@ namespace BulkyWeb.Controllers
             
             _db.Categories.Remove(obj);
             _db.SaveChanges();
-
+            TempData["success"] = "Category deleted successfully!";
             return RedirectToAction("Index");
         }
     }
